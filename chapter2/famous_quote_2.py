ad029ecb036143ee8a00253e031b78f216c48caa
Parsa <parsa@parsa-HP-Laptop-15-ef1xxxgmail.com>
2025-08-07 10:25:59 +0330

    Exercises of Chapter 2 updated.

--- chapter2/famous_quote_2.py
+++ chapter2/famous_quote_2.py
@@ -1,3 +1,3 @@
 famous_name = "Albert Einstein"
-message = f'{famous_name} once said,"A person who never made a mistake never tried anything new."'
+message = f'{famous_name.title()} once said,"A person who never made a mistake never tried anything new."'
 print (message)
