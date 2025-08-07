e40c1d883ef6a2941bef92be012710dd12f8757c
Parsa <parsa@parsa-HP-Laptop-15-ef1xxxgmail.com>
2025-08-07 10:25:23 +0330

    New exercises added to Chapter 2

---
+++ chapter2/famous_quote.py
@@ -0,0 +1,1 @@
+print ('Richard Feynman once sain, "The first principle is that you must not fool yourself — and you are the easiest person to fool."')
--- chapter2/simple_message.py
+++ chapter2/simple_message.py
@@ -1,5 +1,2 @@
-message = "This world is fun"
-print (message)
-
 message = "Hello this is fun"
 print (message)
