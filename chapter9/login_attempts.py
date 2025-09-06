45355f0d2947c2629b59bda0ec48db0c2a5b9109
Parsa <parsapaktinat63@gmail.com>
2025-09-06 12:45:40 +0330

    login_attempts added to the repository.

---
+++ chapter9/login_attempts.py
@@ -0,0 +1,45 @@
+class User:
+    """Model a user."""
+
+    def __init__(self, first_name, last_name, email, age, password):
+        """Initialize attributes."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
+        self.password = password
+        self.login_attempts = 0
+
+    def describe_user(self):
+        """Print a summary of the user's information"""
+        print(f"name: {self.first_name.title()} {self.last_name.title()}")
+        print(f"email: {self.email}")
+        print(f"age: {self.age}")
+        print(f"password: {self.password}\n")
+
+    def greet_user(self):
+        print(f"Hello {self.first_name.title()} {self.last_name.title()}. What's your main goal today?\n")
+
+    def increment_login_attempts(self):
+        """Increment login_attempts by 1"""
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        """Reset the login_attempts to 0"""
+        self.login_attempts = 0
+
+user1 = User("parsa", "paktinat", "parsapaktinat63@gmail.com", 18, "1234rewq")
+# Calling the increment_log_attempts 6 times
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+
+# This line should output 6
+print(user1.login_attempts)
+
+user1.reset_login_attempts()
+# It should be 0 now
+print(user1.login_attempts)
